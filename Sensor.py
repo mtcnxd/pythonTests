e7931d35f6d90dca177d5b138619377bfd030d28
Marcos Tzuc <mtc.nxd@gmail.com>
2024-10-21 15:34:46 -0600

    Insert sensor data in db

--- Sensor.py
+++ Sensor.py
@@ -1,18 +1,25 @@
 from tkinter import *
 import serial # type: ignore
 import requests # type: ignore
-
-puerto = "/dev/ttyUSB0"
-baudrate = 9600
+import pymysql.cursors # type: ignore
 
 class Pycalc(Frame):
-	def __init__(self, master, *args, **kwargs):
-		Frame.__init__(self, master, *args, **kwargs)
+	def __init__(self, master):
+		Frame.__init__(self, master)
 		self.parent = master
 		self.data = ""
+		self.temperature = StringVar()
+		self.Port = serial.Serial(port="/dev/ttyUSB0", baudrate=9600, timeout=5)		
+		
 		self.createWidgets()
-		self.Port = serial.Serial(port=puerto, baudrate=baudrate, timeout=5)
 		self.getTemperature()
+
+		self.connection = pymysql.connect(
+			host="127.0.0.1",
+			user="marcos",
+			password="Tucm+1985",
+			db="mysensors"
+		)
 
 	def createWidgets(self):
 		labelTemperature = Label(text="Temperatura:")
@@ -36,7 +43,7 @@ class Pycalc(Frame):
 		url = "https://io.adafruit.com/api/v2/webhooks/feed/wCTd1eGUXzsVyVUT99TCRhh3qBpZ/raw"
 
 		headers = {
-    		'Content-Type': 'application/octet-stream'
+			'Content-Type': 'application/octet-stream'
 		}
 
 		requests.post(url, headers=headers, data=str(self.data[0]))
@@ -50,10 +57,25 @@ class Pycalc(Frame):
 			self.entryTemperature.delete(0, END)
 			self.entryAltitude.delete(0, END)
 
+			self.temperature = self.data[0]
+
 			self.entryTemperature.insert(0, self.data[0] + " °C")
 			self.entryAltitude.insert(0, self.data[1] + " meters")
+			self.insertData()
 
 		self.after(1000, self.getTemperature)
+
+	def insertData(self):
+		query = "INSERT INTO temperature (location, value) VALUES (%s, %s)"
+		values = ('office', self.temperature)
+		
+		try:
+			cursor = self.connection.cursor()
+			cursor.execute(query, values)
+			self.connection.commit()
+		
+		except:
+			print("Error update data")
 
 
 Termometer = Tk()
