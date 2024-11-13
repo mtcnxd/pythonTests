0e1ba65f6b49cd8b9db7336bf4b536e0b60a5c48
Marcos Tzuc <mtc.nxd@gmail.com>
2024-11-13 11:55:22 -0600

    Testing dynamoDB

---
+++ DynamoDB.py
@@ -0,0 +1,42 @@
+import boto3
+import time
+import random
+from boto3.dynamodb.conditions import Key, Attr
+
+
+data = 0
+
+dynamodb = boto3.resource(
+    'dynamodb', 
+    region_name='us-east-1',
+
+)
+
+while True:
+    print ("Insert data: {}".format(data))
+
+    try:
+        table = dynamodb.Table('SensorLocations')
+        response = table.put_item(
+            Item={
+                'id': random.randint(0,9),
+                'date': str(data),
+                'name': 'Marcos',
+                'lastname': 'Tzuc'
+            }
+        )
+        print("Datos insertados exitosamente:", response)
+    except Exception as e:
+        print("Error al insertar datos:", e)
+
+    time.sleep(1)
+    data = data + 1
+
+    response = table.query(
+        KeyConditionExpression = Key('id').eq(7) & Key('date').eq('5') # , FilterExpression = Attr('date').eq('Marcos')
+    )
+
+
+    print ("-----------------------------------------------------")
+    print (response)
+    print ("-----------------------------------------------------")
