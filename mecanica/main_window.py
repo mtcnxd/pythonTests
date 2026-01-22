6fcbc0714e8c4f531e6a2958fd39a69d8df021b2
mtc.nxd@gmail.com <mtc.nxd@gmail.com>
2026-01-22 14:12:21 -0600

    external class for interface completed

--- mecanica/main_window.py
+++ mecanica/main_window.py
@@ -7,7 +7,38 @@ class Window(Frame):
         self.pack()
 
     def create_widgets(self):
-        pass
+        clients_list_button = Button(self)
+        clients_list_button["text"] = "Get Clients"
+        clients_list_button["command"] = self.show_listbox
+        clients_list_button.pack(side="top")
+
+    def show_listbox(self):
+        json_response = api_client.HttpClient().get_clients()
+        self.clients = json_response['data']
+
+
+        scrollbar = Scrollbar(self.master)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.listbox = Listbox(self.master, height=20, width=100, yscrollcommand=scrollbar.set)
+
+        for client in json_response['data']:
+            self.listbox.insert(END, client['name'])
+
+        self.listbox.pack()
+        self.listbox.bind("<<ListboxSelect>>", self.on_select)
+
+    def on_select(self, event):
+        selected_index = self.listbox.curselection()
+
+        if selected_index:
+            index = selected_index[0]
+            selected_client = self.clients[index]
+            
+            print(f"ID: {selected_client['id']} Name: {selected_client['name']}")
+
+            json_response = api_client.HttpClient().get_client_info(str(selected_client['id']))
+            print(json_response['data'])
 
 
 container = Tk()
