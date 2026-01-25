4806f7cfa17a57df9756e092f78868e4951b37e6
mtc.nxd@gmail.com <mtc.nxd@gmail.com>
2026-01-25 00:06:24 -0600

    Import time to do a script ciclyer

--- bitso/main.py
+++ bitso/main.py
@@ -1,7 +1,10 @@
 from Bitso import Bitso
+import time
 
 bitso = Bitso()
 
 if __name__ == "__main__":
-	book_info = bitso.get_book_info("btc_usdt")
-	print(f"You have selected: {book_info['book']} the current price is: {book_info['last']}")
+	while True:
+		book_info = bitso.get_book_info("btc_usdt")
+		print(f"You have selected: {book_info['book']} => The current price is: {book_info['last']}")
+		time.sleep(10)
