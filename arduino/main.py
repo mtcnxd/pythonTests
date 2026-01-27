b60767a05617630a74c1f5fa08d6910431c62fbd
mtc.nxd@gmail.com <mtc.nxd@gmail.com>
2026-01-27 09:07:21 -0600

    Serial port updated

--- arduino/main.py
+++ arduino/main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from serial import Serial
 
-class Pycalc(Frame):
+class MainWindow(Frame):
 	def __init__(self, master):
 		Frame.__init__(self, master)
 		self.array_data = ""
@@ -15,8 +15,6 @@ class Pycalc(Frame):
 			print("Error al abrir el puerto")
 			return None
 
-		
-		self.entryTemperature = Entry(font=("Arial", 40), borderwidth=1, textvariable=self.temp_value, width=10).place(x=10, y=30)
 		self.createWidgets()
 
 	def __del__(self):
@@ -24,7 +22,11 @@ class Pycalc(Frame):
 
 
 	def createWidgets(self):
-		Label(text="Temperatura:").place(x=10, y=10)
+		Label(text="Temperatura:").grid(row=0, column=0, sticky="w", padx=20, pady=10)
+
+		self.entryTemperature = Entry(font=("Arial", 40, "bold"), borderwidth=1, width=13, textvariable=self.temp_value).grid(row=1, column=0, padx=20, pady=10)
+	
+		Button(text="Save", command=self.func_store_data, padx=40, pady=10).grid(row=2, column=0, sticky="w", padx=20, pady=10)
 
 	def func_get_temp(self):
 		if self.arduino.in_waiting > 0:
@@ -32,15 +34,19 @@ class Pycalc(Frame):
 			decoded_data = bytes_received.decode().rstrip('\r\n')
 			self.array_data = decoded_data.split(',')
 
-			self.temp_value.set(self.array_data[0])
-			print(self.temp_value.get())
+			self.temp_value.set(f"Read value: {self.array_data[0]}")
 
 		self.after(1000, self.func_get_temp)
 
+	def func_store_data(self):
+		print(f"Data: {self.array_data[0]} | Stored successfully")
 
-Termometer = Tk()
-Termometer.title("Sensor BMP180")
-Termometer.geometry('350x250')
 
-root = Pycalc(Termometer)
-Termometer.mainloop()
+if __name__ == "__main__":
+	frame = Tk()
+	frame.title("Arduino Serial")
+	frame.geometry('430x200')
+	frame.resizable(False, False)
+
+	MainWindow(frame)
+	frame.mainloop()
