0f28cc633deb9d34a91cca1bb283e661eed4a5ff
mtc.nxd@gmail.com <mtc.nxd@gmail.com>
2026-02-18 12:52:32 -0600

    Raspberry insert data to database

--- raspberry/DataBase.py
+++ raspberry/DataBase.py
@@ -2,40 +2,37 @@ import pymysql.cursors
 
 class DataBase:
     def __init__(self):
-        self.connection = pymysql.connect(
-            host="127.0.0.1",
-            user="marcos",
-            password="",
-            db="",
-            charset="utf8mb4",
-            cursorclass=pymysql.cursors.DictCursor
-        )
+        try: 
+            self.connection = pymysql.connect(
+                host="127.0.0.1",
+                user="root",
+                password="password",
+                db="BitsoData",
+                charset="utf8mb4",
+            )
+            self.cursor = self.connection.cursor(pymysql.cursors.DictCursor)
+        except e:
+            print("An error ocurred while connecting: {e}")
 
     def __del__(self):
         self.connection.close()
 
-    def select(self, sql, params=None):
+    def select(self, query):
         try:
             with self.connection.cursor() as cursor:
-                cursor.execute(sql, params)
-                result = cursor.fetchall()
-            return result
+                cursor.execute(query)
+                return cursor.fetchall()
 
         except Exception as e:
-            print(f"Error al ejecutar la consulta: {e}")
+            self.connection.rollback()
+            print(f"An error ocurred while execue query: {e}")
             return None
 
     def query(self, query, values):
         try:
-            with self.connection.cursor():
+            with self.connection.cursor() as cursor:
                 cursor.execute(query, values)
-                self.commit()
+                self.connection.commit()
+        
         except Exception as e:
-            print(f"An error ocurred: {e}")
-
-
-'''
-cursor = self.connection.cursor()
-cursor.execute(query, values)
-self.connection.commit()
-'''
+            print(f"An error ocurred while insert data: {e}")
--- raspberry/main.py
+++ raspberry/main.py
@@ -1,10 +1,16 @@
 from DataBase import *
 import time
+from datetime import datetime
 
 if __name__ == "__main__" :
     database = DataBase()
 
     while True:
-        print("Hola mundo")
-        database.query("INSERT INTO table () VALUES ()")
+        now = datetime.now()
+        database.query("INSERT INTO sensors (name, location, update_at, created_at) VALUES (%s,%s,%s,%s)", ('marcos', 'tzuc', now, now))
+
+        result = database.select("SELECT * FROM sensors")
+
+        print(result)
+
         time.sleep(1)
